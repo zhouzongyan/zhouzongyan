fdb7aebf98df214beb30c340707fda861071f7ca
Allen <allen@chn.gg>
2024-06-13 14:22:30 +0800

    添加百度统计js

---
+++ baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e1f89f673cd3c093b9762cc95253fd5b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
